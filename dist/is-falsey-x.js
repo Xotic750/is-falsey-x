df1aad9c709bebdf436521f3f4ed710296f4c438
Graham Fairweather <xotic750@gmail.com>
2019-07-24 01:41:35 +0200

    :bookmark: v2.0.19
    Auto build

--- dist/is-falsey-x.js
+++ dist/is-falsey-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-07-22T12:13:20.029Z",
+  "date": "2019-07-23T23:41:30.417Z",
   "describe": "",
   "description": "Test if a given value is falsey.",
   "file": "is-falsey-x.js",
   "hash": "2fa97319065399c963c1",
   "license": "MIT",
-  "version": "2.0.18"
+  "version": "2.0.19"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
